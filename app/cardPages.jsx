3871528eb7419b17b2b40d8dbf13353f532ba7aa
helen-6545 <122414895+helen-6545@users.noreply.github.com>
2024-11-08 18:45:16 -0500

    test

--- app/cardPages.jsx
+++ app/cardPages.jsx
@@ -1,10 +1,11 @@
 'use client';
 import React from 'react';
 import { useEffect, useState } from 'react'
+import {useRouter} from "next/navigation";
 let readyCards = []
 let drawnCard
 let DataHotel = { 
-  id:0, prompt:"", Answer:"", AnswerReversed: "", image:"" }
+  id:0, prompt:"", Answer:"", AnswerReversed: "", image:"",difficulty:0}
 
 let level
 let showCardButton = true
@@ -16,201 +17,220 @@ let quizPage = true
 let viewPage = false
 let letsStart = false
 
+let cardDifficulty = []
+let cardSchedule = []
+
+
+
+for(let i=0; i<22; i++){
+cardDifficulty.push(3) 
+}
+for(let i=0; i<22; i++){
+cardSchedule.push(0) 
+}
+
 
 
 export default function home(props){
 
+  const router = useRouter();
+
   const data = props.data.map((card, index) =>{
     return {card}})
 
-    console.log(data)
 
     const cardNames = props.data.map((card, index) =>{
       return <div>{card.prompt}</div>
-      //<button className="cardViewButton">
-        //  <label>{card.prompt}</label>
-       // <img className="cardViewImage" src={card.image}/>
-      //</button>
+    })
+    const cardAnswer = props.data.map((card, index) =>{
+      return <div>{card.Answer}</div>
+    })
+    const cardAnswerReversed = props.data.map((card, index) =>{
+      return <div>{card.AnswerReversed}</div>
     })
     const cardImage = props.data.map((card, index) =>{
-      return <div>{card.image}</div>
+      return <img className="cardViewImage" src={card.image}/>
+    })
+    const cardImageDisplay = props.data.map((card, index) =>{
+      return <img className="cardViewImageBig" src={card.image}/>
     })
 
+
+
 const [cardDisplay, setCardDisplay] = useState({
-                   id:0, prompt:"", Answer:"", AnswerReversed: "", image:""});
+                   id:0, prompt:"", Answer:"", AnswerReversed: "", image:"", notes: "", difficulty:0});
 
-                   console.log(cardNames)
-                   console.log(cardImage)
+                
 
-                   return(
-                    <div>
-                      {cardImage[0]}
-                      <button className="cardViewButton" onClick={() => showCard(0)}>
-          <label>{cardNames[0]}</label>
-        <img className="cardViewImage" src={cardImage[0]}/>
-      </button>
-                    </div>
-                   )
-
-  /*return (
+  return (
     <div><text>
 
-<p><button className="linkButtons" onClick={quizPageShow}>
+<p class="center"><button className="linkButtons" onClick={quizPageShow}>
           Quiz
         </button>
         <button className="linkButtons" onClick={viewPageShow}>
           All Cards
         </button></p>
 
+        {!letsStart && <p class="center"><img className="cardViewImageBig" src={"/Images/cardBack.jpg"}/></p>}
+      
+        {quizPage && <div>
 
-      {quizPage && <div>
-        {letsStart && <div>
-      Think about the answer. For added challenge think of the reversed meaning. When you're ready click the button!
+        
+       {letsStart && <div class="center"><div>{letsStart && <>{cardDisplay["image"]}</>}</div>
+
+       <div>{showAnswerButton && <div><p class="center">Think about the answer. </p>
+        <p class="center">For added challenge think of the reversed meaning. </p>
+        <p class="center">When you're ready click the button!</p></div>}
+      {showCheckButtons && <div>Good job! Now mark your answer as right or wrong!</div>}
       <br /> <br />
 
-      <img src = {cardDisplay["image"]}/>
+    
 
-      Your card is number {cardDisplay["id"]}: {cardDisplay["prompt"]}...
+      <div class="center">Your card is number {cardDisplay["id"]}: </div>
+      <div class="center">{cardDisplay["prompt"]}</div>
+      <br /> <br />
 
-
-      <p>{showAnswerButton && <button className="answerButton" onClick={showAnswer}>
+      <p class="center">{showAnswerButton && <button className="answerButton" onClick={showAnswer}>
         Show Answer
       </button>}</p>
 
       {cardDisplay["Answer"]}
-      <br /> <br />
-      {cardDisplay["AnswerReversed"]}</div>}
+      <br/>
+      {cardDisplay["AnswerReversed"]}</div></div>}
 
       <br />
 
-      <p>{showCardButton && <button className="cardButton" onClick={chooseCard}>
+      <p class="center">{showCardButton && <button className="cardButton" onClick={chooseCard}>
         Next Card!
       </button>}</p>
 
       <br />
 
-      <p>{showCheckButtons && <button className="checkButtons" onClick={rightAnswer}>
+      {showCheckButtons &&<p class = "center"><button className="checkButtons" onClick={rightAnswer}>
         Correct!
-      </button>}
-      {showCheckButtons && <button className="checkButtons" onClick={wrongAnswer}>
+      </button>
+      <button className="checkButtons" onClick={wrongAnswer}>
         Wrong...
-      </button>}</p></div>}
+      </button></p>}</div>}
 
       {viewPage && <div>
 
-        {letsStart && <p><img src = {cardDisplay["image"]}/>
-        {cardDisplay["id"]}: {cardDisplay["prompt"]}
+        {letsStart && <div class="center"><div>{cardDisplay["image"]}</div>
+        <p>{cardDisplay["id"]}: {cardDisplay["prompt"]}
         <br />
         {cardDisplay["Answer"]}
         <br /> <br />
         {cardDisplay["AnswerReversed"]}
         <br /> <br />
-        Do you find this card...
+        <p>This card is currently set at level {cardDisplay["difficulty"]} difficulty.</p>
+        <br />
+        <p>Do you find this card...</p>
         <p><button className="checkButtons" onClick={() => easy(cardDisplay["id"])}>
           <label>Easy</label></button>
           <button className="checkButtons" onClick={() => medium(cardDisplay["id"])}>
           <label>Medium</label></button>
           <button className="checkButtons" onClick={() => hard(cardDisplay["id"])}>
-          <label>Hard</label></button></p></p>}
+          <label>Hard</label></button></p></p></div>}
 
-          {cardNames}
+          
         
-        <p><button className="cardViewButton" onClick={() => showCard(0)}>
+        <p class="center"><button className="cardViewButton" onClick={() => showCard(0)}>
           <label>{cardNames[0]}</label>
-        <img className="cardViewImage" src={cardImage[0]}/>
-      </button>
+          {cardImage[0]}
+          </button>
       <button className="cardViewButton" onClick={() => showCard(1)}>
           <label>{cardNames[1]}</label>
-        <img className="cardViewImage" src={cardImage[1]}/>
+          {cardImage[1]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(2)}>
           <label>{cardNames[2]}</label>
-        <img className="cardViewImage" src={cardImage[2]}/>
+          {cardImage[2]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(3)}>
           <label>{cardNames[3]}</label>
-        <img className="cardViewImage" src={cardImage[3]}/>
+          {cardImage[3]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(4)}>
           <label>{cardNames[4]}</label>
-        <img className="cardViewImage" src={cardImage[4]}/>
+          {cardImage[4]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(5)}>
           <label>{cardNames[5]}</label>
-        <img className="cardViewImage" src={cardImage[5]}/>
+        {cardImage[5]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(6)}>
           <label>{cardNames[6]}</label>
-        <img className="cardViewImage" src={cardImage[6]}/>
+          {cardImage[6]}
         </button>
         <button className="cardViewButton" onClick={() => showCard(7)}>
-          <label>{data[7]["prompt"]}</label>
-        <img className="cardViewImage" src={data[7]["image"]}/>
+          <label>{cardNames[7]}</label>
+          {cardImage[7]}
       </button></p>
 
-      <p><button className="cardViewButton" onClick={() => showCard(8)}>
-          <label>{data[8]["prompt"]}</label>
-        <img className="cardViewImage" src={data[8]["image"]}/>
+      <p class="center"><button className="cardViewButton" onClick={() => showCard(8)}>
+          <label>{cardNames[8]}</label>
+          {cardImage[8]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(9)}>
-          <label>{data[9]["prompt"]}</label>
-        <img className="cardViewImage" src={data[9]["image"]}/>
+          <label>{cardNames[9]}</label>
+          {cardImage[9]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(10)}>
-          <label>{data[10]["prompt"]}</label>
-        <img className="cardViewImage" src={data[10]["image"]}/>
+          <label>{cardNames[10]}</label>
+          {cardImage[10]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(11)}>
-          <label>{data[11]["prompt"]}</label>
-        <img className="cardViewImage" src={data[11]["image"]}/>
+          <label>{cardNames[11]}</label>
+          {cardImage[11]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(12)}>
-          <label>{data[12]["prompt"]}</label>
-        <img className="cardViewImage" src={data[12]["image"]}/>
+          <label>{cardNames[12]}</label>
+          {cardImage[12]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(13)}>
-          <label>{data[13]["prompt"]}</label>
-        <img className="cardViewImage" src={data[13]["image"]}/>
+          <label>{cardNames[13]}</label>
+          {cardImage[13]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(14)}>
-          <label>{data[14]["prompt"]}</label>
-        <img className="cardViewImage" src={data[14]["image"]}/>
+          <label>{cardNames[14]}</label>
+          {cardImage[14]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(15)}>
-          <label>{data[15]["prompt"]}</label>
-        <img className="cardViewImage" src={data[15]["image"]}/>
+          <label>{cardNames[15]}</label>
+          {cardImage[15]}
       </button></p>
 
-      <p><button className="cardViewButton" onClick={() => showCard(16)}>
-          <label>{data[16]["prompt"]}</label>
-        <img className="cardViewImage" src={data[16]["image"]}/>
+      <p class="center"><button className="cardViewButton" onClick={() => showCard(16)}>
+          <label>{cardNames[16]}</label>
+          {cardImage[16]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(17)}>
-          <label>{data[17]["prompt"]}</label>
-        <img className="cardViewImage" src={data[17]["image"]}/>
+          <label>{cardNames[17]}</label>
+          {cardImage[17]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(18)}>
-          <label>{data[18]["prompt"]}</label>
-        <img className="cardViewImage" src={data[18]["image"]}/>
+          <label>{cardNames[18]}</label>
+          {cardImage[18]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(19)}>
-          <label>{data[19]["prompt"]}</label>
-        <img className="cardViewImage" src={data[19]["image"]}/>
+          <label>{cardNames[19]}</label>
+          {cardImage[19]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(20)}>
-          <label>{data[20]["prompt"]}</label>
-        <img className="cardViewImage" src={data[20]["image"]}/>
+          <label>{cardNames[20]}</label>
+          {cardImage[20]}
       </button>
       <button className="cardViewButton" onClick={() => showCard(21)}>
-          <label>{data[21]["prompt"]}</label>
-        <img className="cardViewImage" src={data[21]["image"]}/>
+          <label>{cardNames[21]}</label>
+          {cardImage[21]}
       </button></p>
         </div>}
 
 
       
      </text></div>
-  )*/
+  )
 
 
 
@@ -222,21 +242,21 @@ function viewPageShow(){
   showCardButton = true
   letsStart = false
   viewPage=true
-  window.location.reload()
+  router.refresh()
 }
 
 function quizPageShow(){
   quizPage=true
   letsStart = false
   viewPage=false
-  window.location.reload()
+  router.refresh()
 }
 
 function chooseCard(){
   if(data != null){
     readyCards=[]
       for(let i=0; i<22; i++){
-         if (data[i]["scheduledTime"]==0){
+         if (cardSchedule[i]==0){
           readyCards.push(i)
          }
 
@@ -247,32 +267,32 @@ function chooseCard(){
 
       console.log(drawnCard)
       DataHotel["id"] = drawnCard
-      DataHotel["prompt"] = data[drawnCard]["prompt"]
+      DataHotel["prompt"] = cardNames[drawnCard]
       DataHotel["Answer"] = ""
       DataHotel["AnswerReversed"] = ""
-      DataHotel["image"] = data[drawnCard]["image"]
+      DataHotel["image"] = cardImageDisplay[drawnCard]
       setCardDisplay(DataHotel)
       showCardButton = false
       letsStart = true
       showAnswerButton = true
-      window.location.reload()
+      router.refresh()
 }}
 
 function showAnswer(){
   if(data != null){
   DataHotel["id"] = drawnCard
-  DataHotel["prompt"] = data[drawnCard]["prompt"]
-  DataHotel["Answer"] = data[drawnCard]["Answer"]
-  DataHotel["AnswerReversed"] = data[drawnCard]["AnswerReversed"]
-  DataHotel["image"] = data[drawnCard]["image"]
+  DataHotel["prompt"] = cardNames[drawnCard]
+  DataHotel["Answer"] = cardAnswer[drawnCard]
+  DataHotel["AnswerReversed"] = cardAnswerReversed[drawnCard]
+  DataHotel["image"] = cardImageDisplay[drawnCard]
   setCardDisplay(DataHotel)
   showAnswerButton = false
   showCheckButtons = true
-  window.location.reload()
+  router.refresh()
 }}
 
 function rightAnswer(){
-  level = data[drawnCard]["difficulty"]
+  level = cardDifficulty[drawnCard]
   level = level-1;
   if (level==0){
       level=1;
@@ -286,58 +306,69 @@ function wrongAnswer(){
 }
 
 function reset(){
-  data[drawnCard]["difficulty"] = level
+  cardDifficulty[drawnCard] = level
 
   for(let i=0; i<22; i++){
-  if (data[i]["scheduledTime"] != 0) {
-    data[i]["scheduledTime"]=data[i]["scheduledTime"]-1
+  if (cardSchedule[i] != 0) {
+    cardSchedule[i]=cardSchedule[i]-1
 }}
 
 if (level == 1) {
-  data[drawnCard]["scheduledTime"] = 16;
+  cardSchedule[drawnCard] = 16;
 }
 if (level == 2) {
-  data[drawnCard]["scheduledTime"] = 10;
+  cardSchedule[drawnCard] = 10;
 }
 if (level == 3) {
-  data[drawnCard]["scheduledTime"] = 5;
+  cardSchedule[drawnCard] = 5;
 }
 if (level == 4) {
-  data[drawnCard]["scheduledTime"] = 2;
+  cardSchedule[drawnCard] = 2;
 }
 if (level == 5) {
-  data[drawnCard]["scheduledTime"] = 1;
+  cardSchedule[drawnCard] = 1;
 }
 showCheckButtons = false
 showCardButton = true
-window.location.reload()
+router.refresh()
 console.log(data)
 }
 
 function showCard(drawnCard){
   if(data != null){
-    DataHotel["id"] = drawnCard
-    DataHotel["prompt"] = data[drawnCard]["prompt"]
-    DataHotel["Answer"] = data[drawnCard]["Answer"]
-    DataHotel["AnswerReversed"] = data[drawnCard]["AnswerReversed"]
-    DataHotel["image"] = data[drawnCard]["image"]
+  DataHotel["id"] = drawnCard
+  DataHotel["prompt"] = cardNames[drawnCard]
+  DataHotel["Answer"] = cardAnswer[drawnCard]
+  DataHotel["AnswerReversed"] = cardAnswerReversed[drawnCard]
+  DataHotel["image"] = cardImageDisplay[drawnCard]
+  DataHotel["difficulty"] = cardDifficulty[drawnCard]
     setCardDisplay(DataHotel)
     letsStart = true
-    window.location.reload()
+    router.refresh()
   }
 }
 
 function easy(drawnCard){
-  data[drawnCard]["difficulty"] = 1
-  data[drawnCard]["scheduledTime"] = 16;
+  cardDifficulty[drawnCard] = 1
+  cardSchedule[drawnCard] = 16;
+  DataHotel["difficulty"] = cardDifficulty[drawnCard]
+    setCardDisplay(DataHotel)
+  router.refresh()
 }
 function medium(drawnCard){
-  data[drawnCard]["difficulty"] = 3
-  data[drawnCard]["scheduledTime"] = 5;
+  cardDifficulty[drawnCard] = 3
+  cardSchedule[drawnCard] = 5;
+  DataHotel["difficulty"] = cardDifficulty[drawnCard]
+    setCardDisplay(DataHotel)
+  router.refresh()
 }
 function hard(drawnCard){
-  data[drawnCard]["difficulty"] = 5
-  data[drawnCard]["scheduledTime"] = 1;
+  cardDifficulty[drawnCard] = 5
+  cardSchedule[drawnCard] = 1;
+  DataHotel["difficulty"] = cardDifficulty[drawnCard]
+    setCardDisplay(DataHotel)
+    console.log(cardDifficulty)
+  router.refresh()
 }
 
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,8 @@
 'use server'
 import { PrismaClient } from '@prisma/client'
 import Home from './cardPages'
-//let data: {id: number; prompt: string; Answer: string; AnswerReversed: string; image: string; difficulty: number; scheduledTime: number}
-//let cardList: any[] = [];
 const prisma = new PrismaClient()
 
-
-/*export default function find(){
-extract().then(element => {
-return cardList
-})
-}*/
 
 export default async function extract() {
 
