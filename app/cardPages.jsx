a9edfc118e5acdf1d2f5bc5477bd29a3826f5bd9
helen-6545 <122414895+helen-6545@users.noreply.github.com>
2024-11-08 19:09:32 -0500

    test

--- app/cardPages.jsx
+++ app/cardPages.jsx
@@ -38,8 +38,8 @@ export default function Home(props){
 
 
     const cardNames = props.data.map((card, index) => <li key = {index}>{card.prompt}</li>)
-    const cardAnswer = props.data.map((card) => <li key = {index}>{card.Answer}</li>)
-    const cardAnswerReversed = props.data.map((card) => <li key = {index}>{card.AnswerReversed}</li>)
+    const cardAnswer = props.data.map((card,index) => <li key = {index}>{card.Answer}</li>)
+    const cardAnswerReversed = props.data.map((card,index) => <li key = {index}>{card.AnswerReversed}</li>)
     const cardImage = props.data.map((card) =>{
       return <img className="cardViewImage" src={card.image}/>
     })
